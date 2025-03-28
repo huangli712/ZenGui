c62cbe5fb6692b41fd4ccb0976381f4ca3e07420
Li Huang <lihuang.dmft@gmail.com>
2025-03-28 10:11:23 +0800

    fix base.jl

--- src/base.jl
+++ src/base.jl
@@ -6,3 +6,27 @@
 #
 # Last modified: 2025/03/28
 #
+
+function zeng_run()
+    CImGui.set_backend(:GlfwOpenGL3)
+
+    # setup Dear ImGui context
+    ctx = CImGui.CreateContext()
+
+    # enable docking and multi-viewport
+    setup_config_flags()
+
+    # When viewports are enabled we tweak WindowRounding/WindowBg so platform
+    # windows can look identical to regular ones.
+    tweak_window()
+
+    # Load Fonts
+    setup_fonts()
+
+    clear_color = Cfloat[0.45, 0.55, 0.60, 1.00]
+    engine = nothing
+
+    CImGui.render(ctx; engine, clear_color=Ref(clear_color), window_title = "ZenGui") do
+        create_main_menu()
+    end
+end
