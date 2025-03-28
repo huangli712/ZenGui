2278a95af141a7300b3973cd09442e16effc600a
Li Huang <lihuang.dmft@gmail.com>
2025-03-28 10:01:26 +0800

    update ZenGui

--- src/ZenGui.jl
+++ src/ZenGui.jl
@@ -4,5 +4,5 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/03/25
+# Last modified: 2025/03/28
 #
--- src/global.jl
+++ src/global.jl
@@ -1,10 +1,10 @@
 #
-# Project : Lily
-# Source  : global.jl
+# Project : Camellia
+# Source  : ZenGui.jl
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/01/21
+# Last modified: 2025/03/28
 #
 
 #=
