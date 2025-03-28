307668ac00978bb262c86f215c42e5f01aada9c8
Li Huang <lihuang.dmft@gmail.com>
2025-03-28 10:07:26 +0800

    fix ZenGui.jl

--- src/ZenGui.jl
+++ src/ZenGui.jl
@@ -7,5 +7,28 @@
 # Last modified: 2025/03/28
 #
 
+"""
+    ZenGui
+
+"""
 module ZenGui
+
+include("global.jl")
+
+include("util.jl")
+
+include("menu.jl")
+
+include("zen.jl")
+include("ctseg.jl")
+include("cthyb.jl")
+include("atomic.jl")
+
+include("acflow.jl")
+include("actest.jl")
+
+include("about.jl")
+
+include("base.jl")
+
 end
