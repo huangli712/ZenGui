ee415ca69ee9a2d9698cbbd76b49461722094e6f
Li Huang <lihuang.dmft@gmail.com>
2025-03-31 21:05:11 +0800

    update ZenGui

--- src/acflow.jl
+++ src/acflow.jl
@@ -16,26 +16,62 @@ function create_app_acflow(p_open::Ref{Bool})
     )
 
     # Fix size of the window
-    window_width = 500.0
-    window_height = 800.0
+    window_width = 600.0
+    window_height = 600.0
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
 
+
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Filename for input data")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Solver for the analytic continuation problem")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Type of kernel function")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Type of default model function")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Grid for input data (imaginary axis)")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Mesh for output data (real axis)")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Number of grid points")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Number of mesh points")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Right boundary (maximum value) of output mesh")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Left boundary (minimum value) of output mesh")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Inverse temperature")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Is it the offdiagonal part in matrix-valued function")
+    #
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
+    CImGui.SameLine()
     CImGui.Text("Are the analytic continuation results written into files")
-    CImGui.Text("")
-    CImGui.Text("")
-    CImGui.Text("")
 
     # End of this window
     CImGui.End()
