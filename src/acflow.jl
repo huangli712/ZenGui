4ff059f944ad1f681789f08b0b32ce12ad90952e
Li Huang <lihuang.dmft@gmail.com>
2025-03-31 22:03:41 +0800

    update ZenGui

--- src/acflow.jl
+++ src/acflow.jl
@@ -21,62 +21,103 @@ function create_app_acflow(p_open::Ref{Bool})
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
 
     widget_input_width = 100
+    widget_combo_width = 100
 
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "finput :")
     CImGui.SameLine()
     CImGui.Text("Filename for input data")
     CImGui.SameLine()
     CImGui.SetNextItemWidth(widget_input_width)
-    finput = "giw.data"
-    CImGui.InputText("", finput, length(finput))
+    @cstatic finput = "giw.data" begin
+        CImGui.InputText("T1", finput, length(finput) * 2)
+    end
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "solver :")
     CImGui.SameLine()
     CImGui.Text("Solver for the analytic continuation problem")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_combo_width)
+    solvers = ["MaxEnt", "BarRat", "NevanAC", "StochAC", "StochSK", "StochOM", "StochPX"]
+    @cstatic solver_id = Cint(0) begin
+        @c CImGui.Combo("C1", &solver_id, solvers)
+    end
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "ktype  :")
     CImGui.SameLine()
     CImGui.Text("Type of kernel function")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "mtype  :")
     CImGui.SameLine()
     CImGui.Text("Type of default model function")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "grid   :")
     CImGui.SameLine()
     CImGui.Text("Grid for input data (imaginary axis)")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "mesh   :")
     CImGui.SameLine()
     CImGui.Text("Mesh for output data (real axis)")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "ngrid  :")
     CImGui.SameLine()
     CImGui.Text("Number of grid points")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "nmesh  :")
     CImGui.SameLine()
     CImGui.Text("Number of mesh points")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "wmax   :")
     CImGui.SameLine()
     CImGui.Text("Right boundary (maximum value) of output mesh")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "wmin   :")
     CImGui.SameLine()
     CImGui.Text("Left boundary (minimum value) of output mesh")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "beta   :")
     CImGui.SameLine()
     CImGui.Text("Inverse temperature")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "offdiag:")
     CImGui.SameLine()
     CImGui.Text("Is it the offdiagonal part in matrix-valued function")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "fwrite :")
     CImGui.SameLine()
     CImGui.Text("Are the analytic continuation results written into files")
+    CImGui.SameLine()
+    CImGui.SetNextItemWidth(widget_input_width)
+    CImGui.Text("TODO")
     #
     CImGui.Spacing()
     CImGui.Separator()
