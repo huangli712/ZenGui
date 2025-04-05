be484d929f76526d403572f8d91b62dce5a4899c
Li Huang <lihuang.dmft@gmail.com>
2025-04-05 22:59:37 +0800

    fix zeng.jl

--- util/zeng.jl
+++ util/zeng.jl
@@ -1,6 +1,10 @@
 #!/usr/bin/env julia
 
 #
+# This script is used to launch the ZenGui app. To make it work correctly,
+# please make sure that the ZenGui is correctly installed via `Pkg.add()`,
+# or the environment variable `ZEN_GUI` is correctly set. It should point
+# to the `zengui/src` directory.
 #
 # Usage:
 #
