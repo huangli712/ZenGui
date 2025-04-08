b387f4bc10142c385b0ac03d1a3573f51064a44e
Li Huang <lihuang.dmft@gmail.com>
2025-04-08 22:46:41 +0800

    fix save.jl

--- src/save.jl
+++ src/save.jl
@@ -7,7 +7,23 @@
 # Last modified: 2025/04/08
 #
 
+"""
+    save_acflow()
+"""
 function save_acflow()
+    CImGui.OpenPopup("Save")
+    #
+    if CImGui.BeginPopupModal("Save", C_NULL, CImGui.ImGuiWindowFlags_AlwaysAutoResize)
+        file = joinpath(pwd(), "ac.toml")
+        CImGui.Text("The configurtion file for ACFlow will be saved at $file.")
+        #
+        if CImGui.Button("OK")
+            CImGui.CloseCurrentPopup()
+        end
+        #
+        CImGui.EndPopup()
+    end
+    #
     D = _build_acflow_dict()
     open("ac.toml", "w") do fout
         TOML.print(fout, D)
