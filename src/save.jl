e9762f1dbee033d865a06794ddb0c6d54921b52c
Li Huang <lihuang.dmft@gmail.com>
2025-04-08 23:05:50 +0800

    fix save.jl

--- src/save.jl
+++ src/save.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/04/08
+# Last modified: 2025/04/09
 #
 
 """
@@ -12,23 +12,23 @@
 """
 function save_acflow(p_open::Ref{Bool})
     CImGui.Begin(
-        "Save",
+        "Save ACFlow",
         p_open,
         CImGui.ImGuiWindowFlags_Modal | CImGui.ImGuiWindowFlags_NoResize
     )
 
-        file = joinpath(pwd(), "ac.toml")
-        CImGui.Text("The configurtion file for ACFlow will be saved at $file.")
-        #
-        if CImGui.Button("OK")
-            p_open[] = false
-            D = _build_acflow_dict()
-            open("ac.toml", "w") do fout
-                TOML.print(fout, D)
-            end
-        end
-        #
-    CImGui.End()
-    #
+    file = joinpath(pwd(), "ac.toml")
+    CImGui.Text("The configurtion file for ACFlow will be saved at:")
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "  $file")
 
+    if CImGui.Button("Save It")
+        p_open[] = false
+        #
+        D = _build_acflow_dict()
+        open("ac.toml", "w") do fout
+            TOML.print(fout, D)
+        end
+    end
+
+    CImGui.End()
 end
