f6e6da78998e9d877132362461bfa5972b764b13
Li Huang <lihuang.dmft@gmail.com>
2025-04-09 21:54:21 +0800

    fix zen.jl

--- src/zen.jl
+++ src/zen.jl
@@ -31,7 +31,23 @@ function create_app_zen(p_open::Ref{Bool})
     window_height = 300.0
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
 
-    CImGui.Text("Zen")
+    tab_bar_flags = CImGui.ImGuiTabBarFlags_None
+    if CImGui.BeginTabBar("MyTabBar", tab_bar_flags)
+        if CImGui.BeginTabItem("Avocado")
+            CImGui.Text("This is the Avocado tab!\nblah blah blah blah blah")
+            CImGui.EndTabItem()
+        end
+        if CImGui.BeginTabItem("Broccoli")
+            CImGui.Text("This is the Broccoli tab!\nblah blah blah blah blah")
+            CImGui.EndTabItem()
+        end
+        if CImGui.BeginTabItem("Cucumber")
+            CImGui.Text("This is the Cucumber tab!\nblah blah blah blah blah")
+            CImGui.EndTabItem()
+        end
+        CImGui.EndTabBar()
+    end
+
 
     # End of this window
     CImGui.End()
