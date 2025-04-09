4b862eda68c3fd96fd05cd70ebb680e7908be9d8
Li Huang <lihuang.dmft@gmail.com>
2025-04-09 22:47:05 +0800

    fix save_zen()

--- src/save.jl
+++ src/save.jl
@@ -4,14 +4,37 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/04/09
+# Last modified: 2025/04/10
 #
 
 """
     save_zen(p_open::Ref{Bool})
 """
 function save_zen(p_open::Ref{Bool})
+    # Create a popup window
+    CImGui.Begin(
+        "Save Zen",
+        p_open,
+        CImGui.ImGuiWindowFlags_Modal | CImGui.ImGuiWindowFlags_NoResize
+    )
 
+    file = joinpath(pwd(), "case.toml")
+    CImGui.Text("The configurtion file for Zen will be saved at:")
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "  $file")
+
+    # If the button is pressed, then the ac.toml file is stored in the
+    # current directory.
+    if CImGui.Button("Save It")
+        p_open[] = false
+        #
+        D = _build_zen_dict()
+        open("case.toml", "w") do fout
+            TOML.print(fout, D)
+        end
+    end
+
+    # Close the popup window
+    CImGui.End()
 end
 
 """
