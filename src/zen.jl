a5d1452a641a030610ad1d7368eaff24c9f670b4
Li Huang <lihuang.dmft@gmail.com>
2025-04-11 16:50:56 +0800

    fix zen.jl

--- src/zen.jl
+++ src/zen.jl
@@ -607,10 +607,20 @@ function _zen_imp_block()
 
         for i = 1:PIMP.nsite
 
+            CImGui.SetNextItemWidth(widget_input_width)
+
             i == 1 && @_widgets_generator 1
             i == 2 && @_widgets_generator 2
+            i == 3 && @_widgets_generator 3
+            i == 4 && @_widgets_generator 4
+            i == 5 && @_widgets_generator 5
+            i == 6 && @_widgets_generator 6
+            i == 7 && @_widgets_generator 7
+            i == 8 && @_widgets_generator 8
+            i == 9 && @_widgets_generator 9
 
         end
+        
         @show PIMP.atoms
         CImGui.EndTabItem()
     end
