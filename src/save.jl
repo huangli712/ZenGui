df04310dc86e81a3b205781dd6dd4f95cc6b38ff
Li Huang <lihuang.dmft@gmail.com>
2025-04-15 17:51:36 +0800

    implement save_atomic()

--- src/save.jl
+++ src/save.jl
@@ -119,7 +119,32 @@ end
     save_atomic(p_open::Ref{Bool})
 """
 function save_atomic(p_open::Ref{Bool})
+    # Create a popup window
+    CImGui.Begin(
+        "Save iQIST | atomic",
+        p_open,
+        CImGui.ImGuiWindowFlags_Modal | CImGui.ImGuiWindowFlags_NoResize
+    )
 
+    file = joinpath(pwd(), "solver.atomic.in")
+    CImGui.Text("The configurtion file for iQIST/atomic will be saved at:")
+    CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "  $file")
+
+    # If the button is pressed, then the solver.atomic.in file is stored
+    # in the current directory.
+    if CImGui.Button("Save It")
+        p_open[] = false
+        #
+        D = _build_iqist_dict("atomic")
+        open("solver.atomic.in", "w") do fout
+            for (key, value) in D
+                println(fout, "$key = $value")
+            end            
+        end
+    end
+
+    # Close the popup window
+    CImGui.End()
 end
 
 """
