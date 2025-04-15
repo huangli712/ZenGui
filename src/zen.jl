5a2be96a45686a485d473db031b92582f0546b2d
Li Huang <lihuang.dmft@gmail.com>
2025-04-15 15:45:48 +0800

    fix _ze_solver_block()

--- src/zen.jl
+++ src/zen.jl
@@ -620,6 +620,15 @@ function _zen_solver_block()
         end
         CImGui.SameLine()
         CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "(engine)$(PSOLVER.engine)")
+        #
+        # Input: ncycle
+        CImGui.SetNextItemWidth(widget_input_width)
+        @cstatic _i = Cint(8) begin
+            @c CImGui.InputInt(" Number of solver iterations per DFT + DMFT cycle", &_i)
+            PIMP.ncycle = _i
+        end
+        CImGui.SameLine()
+        CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "(ncycle)$(PIMP.ncycle)")
 
         CImGui.EndTabItem()
     end
