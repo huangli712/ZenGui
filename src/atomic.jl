c01c3118bf2c2fa2c2c86209d443096dad42fc32
Li Huang <lihuang.dmft@gmail.com>
2025-04-16 20:56:50 +0800

    fix create_app_atomic()

--- src/atomic.jl
+++ src/atomic.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/04/09
+# Last modified: 2025/04/16
 #
 
 """
@@ -14,9 +14,9 @@ Create an UI window for the atomic code, which is an atomic eigenvalue
 problem solver in the iQIST package.
 """
 function create_app_atomic(p_open::Ref{Bool})
-    # Create the atomic window, which is modal and can not be resized.
+    # Create the atomic window, which can not be resized.
     CImGui.Begin(
-        "iQIST | ATOMIC",
+        "iQIST | atomic",
         p_open,
         CImGui.ImGuiWindowFlags_NoResize
     )
@@ -27,11 +27,20 @@ function create_app_atomic(p_open::Ref{Bool})
     end
 
     # Fix size of the window
-    window_width = 400.0
-    window_height = 300.0
+    window_width = 600.0
+    window_height = 600.0
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
 
-    CImGui.Text("ATOMIC")
+    # For all the blocks in the solver.atomic.in
+    _atomic_tabs_block()
+
+    # For the separator
+    CImGui.Spacing()
+    CImGui.Separator()
+    CImGui.Spacing()
+
+    # For the buttons in the bottom of this window
+    _atomic_bottom_block(p_open)
 
     # End of this window
     CImGui.End()
