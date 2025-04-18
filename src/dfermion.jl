e062f21542fc64f41e314fe129bef79db36c3e6c
Li Huang <lihuang.dmft@gmail.com>
2025-04-18 15:28:00 +0800

    fix dfermion.jl

--- src/dfermion.jl
+++ src/dfermion.jl
@@ -14,7 +14,7 @@ Create an UI window for the DFermion code, which is a dual fermion theory
 engine.
 """
 function create_app_dfermion(p_open::Ref{Bool})
-    # Create the DFermion window, which is modal and can not be resized.
+    # Create the DFermion window, which can not be resized.
     CImGui.Begin(
         "DFermion",
         p_open,
@@ -27,11 +27,9 @@ function create_app_dfermion(p_open::Ref{Bool})
     end
 
     # Fix size of the window
-    window_width = 400.0
-    window_height = 300.0
+    window_width = 600.0
+    window_height = 600.0
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
-
-    CImGui.Text("DFermion")
 
     # End of this window
     CImGui.End()
