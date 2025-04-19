84d79eb69f527ef8270bb794376d974952cdc916
Li Huang <lihuang.dmft@gmail.com>
2025-04-19 23:56:00 +0800

    fix comments

--- src/about.jl
+++ src/about.jl
@@ -4,14 +4,14 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/04/15
+# Last modified: 2025/04/19
 #
 
 """
     create_app_about(p_open::Ref{Bool})
 
 Display the `About` window, which is used to show some userful information
-for users.
+about this ZenGui app.
 """
 function create_app_about(p_open::Ref{Bool})
     # Create the About window, which is modal and can not be resized.
@@ -26,8 +26,8 @@ function create_app_about(p_open::Ref{Bool})
     window_height = 300.0
     CImGui.SetWindowSize(ImVec2(window_width, window_height))
 
-    # Below are some widges
-
+    # Show header
+    #
     # We want to make sure `ZenGui` is shown in the middle of the window.
     txt_width = CImGui.CalcTextSize("ZenGui").x
     offset = (window_width - txt_width) / 2.0
@@ -37,48 +37,57 @@ function create_app_about(p_open::Ref{Bool})
     CImGui.Spacing()
     CImGui.TextWrapped("A general-purposed graphic user interface for " *
         "ab initio dynamical mean-field theory codes")
-    #
+
+    # Vertical space
     CImGui.Spacing()
     CImGui.Separator()
     CImGui.Dummy(ImVec2(0.0,10.0))
 
+    # Show author
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "Author :")
     CImGui.SameLine()
     CImGui.Text(__AUTHORS__[1].name)
-    #
+
+    # Show email
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "Contact:")
     CImGui.SameLine()
     CImGui.Text(__AUTHORS__[1].email)
-    #
+
+    # Show version
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "Version:")
     CImGui.SameLine()
     CImGui.Text("v$(__VERSION__)")
-    #
+
+    # Show license
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "License:")
     CImGui.SameLine()
     CImGui.Text("GNU General Public License Version 3")
-    #
+
+    # Show github url
     CImGui.TextColored(ImVec4(1.0,0.0,1.0,1.0), "Github :")
     CImGui.SameLine()
     CImGui.Text("https://github.com/huangli712/ZenGui")
 
+    # Show footer
     CImGui.Spacing()
     CImGui.TextWrapped("Powered by the Julia language (v$VERSION) " *
         "and the Dear ImGui library (v$(CImGui.IMGUI_VERSION)).")
+    CImGui.Dummy(ImVec2(0.0,10.0))
 
     # Create a `OK` button. It will reset `p_open`.
-    CImGui.Dummy(ImVec2(0.0,10.0))
+    #
+    # Set button's geometry
+    button_width = 80.0
+    button_height = 25.0
     #
     # Change the default color for the button
     CImGui.PushStyleColor(CImGui.ImGuiCol_Button, ImVec4(1.0,0.0,1.0,1.0))
     #
-    button_width = 80.0
-    button_height = 25.0
     if CImGui.Button("OK", ImVec2(button_width, button_height))
         p_open[] = false
     end
     #
-    # Reset the color
+    # Reset the default color
     CImGui.PopStyleColor(1)
 
     # End of this window
