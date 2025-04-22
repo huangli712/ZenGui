4e1559b24d7a4fc3babd140b43b45fa7f14677c2
Li Huang <lihuang.dmft@gmail.com>
2025-04-22 23:00:41 +0800

    fix ctseg.jl

--- src/ctseg.jl
+++ src/ctseg.jl
@@ -127,10 +127,9 @@ function _ctseg_bottom_block(p_open::Ref{Bool})
     end
     #
     if CImGui.BeginPopupModal("View", C_NULL, CImGui.ImGuiWindowFlags_AlwaysAutoResize)
-        @cstatic read_only=false text="Hello World!" begin
-            text = _dict_to_toml(_build_iqist_dict("ctseg"))
-            @c CImGui.Checkbox("Read-only", &read_only)
-            flags = read_only ? CImGui.ImGuiInputTextFlags_ReadOnly : 0
+        @cstatic text="Hello World!" begin
+            text = _dict_to_string(_build_iqist_dict("ctseg"))
+            flags = CImGui.ImGuiInputTextFlags_ReadOnly
             flags = CImGui.ImGuiInputTextFlags_AllowTabInput | flags
             CImGui.InputTextMultiline("##source", text, 10000, ImVec2(400, 600), flags)
         end
