0d8d1b30c6181d5b8e39fed79c3763bcc8f8bde5
Li Huang <lihuang.dmft@gmail.com>
2025-04-25 00:42:04 +0800

    fix colors

--- src/base.jl
+++ src/base.jl
@@ -193,7 +193,7 @@ function setup_window()
         )
     end
     #
-    # Setup default colors for buttons 
+    # Setup default colors for buttons
     CImGui.c_set!(style.Colors, CImGui.ImGuiCol_Button, COL_ORANGE)
     CImGui.c_set!(style.Colors, CImGui.ImGuiCol_ButtonHovered, COL_LIGHTGREEN)
     #
