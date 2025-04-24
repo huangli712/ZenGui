c98af07963e9db9b6fdd3adb7158251812d00f92
Li Huang <lihuang.dmft@gmail.com>
2025-04-24 20:22:17 +0800

    fix _zen_bottom_block()

--- src/zen.jl
+++ src/zen.jl
@@ -237,10 +237,9 @@ function _zen_bottom_block(p_open::Ref{Bool})
     end
     #
     if CImGui.BeginPopupModal("View", C_NULL, CImGui.ImGuiWindowFlags_AlwaysAutoResize)
-        @cstatic read_only=false text="Hello World!" begin
+        @cstatic text="Hello World!" begin
             text = dict_to_toml(_build_zen_dict())
-            @c CImGui.Checkbox("Read-only", &read_only)
-            flags = read_only ? CImGui.ImGuiInputTextFlags_ReadOnly : 0
+            flags = CImGui.ImGuiInputTextFlags_ReadOnly
             flags = CImGui.ImGuiInputTextFlags_AllowTabInput | flags
             CImGui.InputTextMultiline("##source", text, 10000, ImVec2(400, 600), flags)
         end
