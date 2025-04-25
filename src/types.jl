e2c5647d4b6df2ea10c9dd3612357968d9391d7f
Li Huang <lihuang.dmft@gmail.com>
2025-04-25 14:56:33 +0800

    fix colors

--- src/types.jl
+++ src/types.jl
@@ -17,6 +17,9 @@ const COL_PURPLE     = ImVec4(0.50, 0.00, 0.50, 1.00)
 const COL_PINK       = ImVec4(1.00, 0.75, 0.80, 1.00)
 const COL_SKYBLUE    = ImVec4(0.50, 0.80, 1.00, 1.00)
 const COL_LIGHTGREEN = ImVec4(0.50, 0.90, 0.50, 1.00)
+const COL_TEAL       = ImVec4(0.00, 0.50, 0.50, 1.00)
+const COL_BEIGE      = ImVec4(0.96, 0.96, 0.86, 1.00)
+const COL_INDIGO     = ImVec4(0.29, 0.00, 0.51, 1.00)
 
 #=
 ### *Customized Structs* : *Active Window*
